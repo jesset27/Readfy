d82bdbdda9641e16fb87c9cac650c4573247eabc
Jessé Willian <jessewillian0@gmail.com>
2023-05-23 19:40:27 -0300

    1.0

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php require "src/Views/layout/header.php"; ?>
 <div class="container">
-    <form action="#" method="post">
-        <label for="email">Email</label>
-    </form>
+    
 </div>
 <?php require "src/Views/layout/footer.php"; ?>
