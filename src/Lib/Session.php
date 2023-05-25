a661a465f44bc267d81e41315c463193be2863bd
Jessé Willian <90875367+jesset27@users.noreply.github.com>
2023-05-24 21:20:52 -0300

    1.0

---
+++ src/Lib/Session.php
@@ -0,0 +1,22 @@
+<?php  
+    class Session(){
+        public function __construct(){
+            session_start();
+        }
+        public function defineValor($chave, $valor){
+            $_SESSION[$chave] = $valor;
+        }
+        public function obterValor(){
+            if (isset($_SESSION[$chave])){
+                return $_SESSION[$chave];
+            }
+            return null;
+        }
+        public function removerValor($chave){
+            unset($_SESSION[$chave]);
+        }
+        public function encerrar(){
+            session_destroy();
+        }
+    }
+?>
