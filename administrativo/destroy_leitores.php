fde60297e4c645a69359f0df61870018fc0916a0
Jessé Willian <jessewillian0@gmail.com>
2023-11-06 17:32:43 -0300

    1.0

--- administrativo/destroy_leitores.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $id = $_GET['id'];
-    require_once('../src/Models/ClassesDAO/LeitorDao.php');
-    require_once('../src/Lib/connect.php');
-    $leitorDao = new LeitorDAO($pdo);
-    $leitorDao->deletar($id);
-    header("Location: ./leitores.php");
-?>
