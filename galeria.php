abca21fc2b2fafdd07087200cd7972ed57e62aab
evandroribeirogomes <evribeirgo_07@hotmail.com>
2023-11-24 03:32:01 -0300

    galeria

---
+++ galeria.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galeria de Livros</title>
+    <!-- Inclua os arquivos CSS do Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public\css\style_galeria.css">
+</head>
+
+<body>
+    <header class="header">
+
+        <div class="logo">
+            <img src="\Readfy\public\img\logo6.png ">
+        </div>
+
+        <nav class="navbar">
+           
+        <div class="dropdown">
+        <button class="dropbtn">Categorias</button>
+        <div class="dropdown-content">
+            <a href="#">Ação</a>
+            <a href="#"> Comédia</a></a>
+            <a href="#">Suspence</a>
+            <!-- Adicione mais categorias conforme necessário -->
+        </div>
+    </div>
+    <a href="sobre.php">Sair</a>
+        </nav>
+
+    </header>
+
+    <div class="container mt-5">
+        <h2 class="mb-4">Galeria de Livros</h2>
+
+        <div class="row">
+            <!-- Livro 1 -->
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="public\img\capas\themagicians.jpg" class="card-img-top" alt="Livro 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Livro 1</h5>
+                        <a href="#" class="btn btn-primary">Ler</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Livro 2 -->
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="public\img\capas\themagicians.jpg" class="card-img-top" alt="Livro 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Livro 2</h5>
+                        <a href="#" class="btn btn-primary">Ler</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Livro 3 -->
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="public\img\capas\themagicians.jpg" class="card-img-top" alt="Livro 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Livro 3</h5>
+                        <a href="#" class="btn btn-primary">Ler</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Livro 4 -->
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="public\img\capas\themagicians.jpg" class="card-img-top" alt="Livro 4">
+                    <div class="card-body">
+                        <h5 class="card-title">Livro 4</h5>
+                        <a href="#" class="btn btn-primary">Ler</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="public\img\capas\themagicians.jpg" Romance Pai e Filho Capa para Livro (3).jpg class="card-img-top" alt="Livro 4">
+                    <div class="card-body">
+                        <h5 class="card-title">Livro 4</h5>
+                        <a href="#" class="btn btn-primary">Ler</a>
+                    </div>
+                </div>
+                          </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="public\img\capas\themagicians.jpg" class="card-img-top" alt="Livro 4">
+                    <div class="card-body">
+                        <h5 class="card-title">Livro 4</h5>
+                        <a href="#" class="btn btn-primary">Ler</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+
+    <!-- Inclua os arquivos JavaScript do Bootstrap (opcional) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
